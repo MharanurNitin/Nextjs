e088c46dafe34357dd37eb4f95479d7ebf917b56
Nitin-Mharanur <nitin.mharanur@rasonix.com>
2023-10-04 11:55:54 +0530

    added nested routing

---
+++ src/app/login/loginTeacher/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TeacherLogin = () => {
+  return <div>TeacherLogin page</div>;
+};
+
+export default TeacherLogin;
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div>Login</div>
+      <br />
+      <Link href={"/login/loginTeacher"}> Teacher Login</Link>
+      <br />
+      <Link href={"/login/loginStudent"}> Student Login</Link>
+    </>
+  );
 };
 
 export default Login;
